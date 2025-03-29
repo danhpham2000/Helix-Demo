a29b8ee27d1fbf3e1d7f2b22142c1da4fcdd2aba
Danh Pham <115748225+danhpham2000@users.noreply.github.com>
2025-03-29 03:11:22 -0500

    Add message of sequence to agent

--- helix-backend/agents.py
+++ helix-backend/agents.py
@@ -1,19 +1,17 @@
-from typing import Annotated
-from typing_extensions import TypedDict
-from langchain_core.tools import tool
-from langchain_core.output_parsers import JsonOutputParser
-from langchain_core.messages import HumanMessage, AIMessage, BaseMessage
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings, OpenAI
-from langgraph.checkpoint.memory import MemorySaver
-from langgraph.prebuilt import ToolNode, tools_condition
-from langgraph.graph import MessagesState, END, START, StateGraph
-from langgraph.graph.message import add_messages
-from langchain.prompts import ChatPromptTemplate
-from dotenv import load_dotenv
 import os
+from typing import Annotated
+
+from dotenv import load_dotenv
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, StateGraph
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+from typing_extensions import TypedDict
+
 from database import connection, cursor
-import requests
-from sql import INSERT_INTO_SEQUENCE_TABLE, UPDADATE_INTO_SEQUENCE_TABLE, GET_SEQUENCE_DATA
+from sql import INSERT_INTO_SEQUENCE_TABLE, UPDATE_INTO_SEQUENCE_TABLE
 
 load_dotenv()
 
@@ -21,9 +19,6 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 
 memory = MemorySaver()
 
-sequence_id = 0
-
-openai_model = ChatOpenAI(model="gpt-4o", api_key=openai_api_key, temperature=0.5)
 
 @tool
 def generate_sequence(num_steps: int, description: list, title: str):
@@ -38,9 +33,10 @@ def generate_sequence(num_steps: int, description: list, title: str):
     """
     try:
         cursor.execute(INSERT_INTO_SEQUENCE_TABLE, (title, num_steps, description))
+        cursor.execute("SELECT lastval()")
         sequence_id = cursor.fetchone()[0]
         print("This is the id of recent sequence", sequence_id)
-        
+
         connection.commit()
 
         print("Sequence add ok")
@@ -48,7 +44,8 @@ def generate_sequence(num_steps: int, description: list, title: str):
     except Exception as e:
         print(e)
         return e
-    
+
+
 @tool
 def update_sequence(title: str, num_steps: int, description: list):
     """This is for update the sequence data directly by AI and request by user
@@ -62,7 +59,10 @@ def update_sequence(title: str, num_steps: int, description: list):
 
     print("Begin update state...")
     try:
-        cursor.execute(UPDADATE_INTO_SEQUENCE_TABLE, (title, num_steps, description, "8"))
+        cursor.execute("SELECT lastval()")
+        sequence_id = cursor.fetchone()[0]
+        print("This is the id of recent sequence", sequence_id)
+        cursor.execute(UPDATE_INTO_SEQUENCE_TABLE, (title, num_steps, description, sequence_id))
         print("Sequence update ok")
         connection.commit()
 
@@ -70,18 +70,34 @@ def update_sequence(title: str, num_steps: int, description: list):
     except Exception as e:
         print(e)
         return e
-    
+
+
 class State(TypedDict):
     messages: Annotated[list, add_messages]
+    title: str
+    num_steps: int
+    description: list[str]
+
 
 workflow = StateGraph(State)
 
 tools = [generate_sequence, update_sequence]
-llm = ChatOpenAI(api_key=openai_api_key, model="gpt-4o")
+llm = ChatOpenAI(model="gpt-4o", temperature=0, api_key=openai_api_key)
 llm_with_tools = llm.bind_tools(tools)
 
+
+
 def chatbot(state: State):
+    response = llm_with_tools.invoke(state["messages"])
+
+    if "GENERATE SEQUENCE..." in response:
+        return {"messages": [llm_with_tools.invoke(state["messages"])],
+                "sequences": response.additional_kwargs.get("tool_calls")[0].get("function").get("arguments")}
+    elif "UPDATE SEQUENCE..." in response:
+        return {"messages": [llm_with_tools.invoke(state["messages"])],
+                "sequences": response.additional_kwargs.get("tool_calls")[0].get("function").get("arguments")}
     return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
 
 workflow.add_node("chatbot", chatbot)
 tool_node = ToolNode(tools=[generate_sequence, update_sequence])
@@ -94,12 +110,11 @@ workflow.add_edge(START, "chatbot")
 
 workflow_graph = workflow.compile(checkpointer=memory)
 
-config = {"configurable": {"thread_id": "1"}}
-
-
-# user_input = "Write a HR sequence targeting candidates in software engineer involve email, offer letter, and interview"
-
-
+# config = {"configurable": {"thread_id": "1"}}
+#
+# user_input = "Write a HR short message outreach targeting candidates in software engineer involve email, offer letter, and interview"
+#
+#
 # events = workflow_graph.stream(
 #     {
 #         "messages": [{"role": "user", "content": user_input}]
@@ -107,13 +122,6 @@ config = {"configurable": {"thread_id": "1"}}
 #     config=config,
 #     stream_mode="values"
 # )
-
+#
 # for event in events:
 #     event["messages"][-1].pretty_print()
-
-
-
-
-
-
-
