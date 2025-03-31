78b2e312fb4cdab67ab0bb52377972f3b45606e6
Danh Pham <115748225+danhpham2000@users.noreply.github.com>
2025-03-31 02:26:03 -0500

    Add sign up flow for chat

--- helix-front/src/components/Chat/Chat.tsx
+++ helix-front/src/components/Chat/Chat.tsx
@@ -12,17 +12,27 @@ const testSocket = new WebSocket(
 );
 
 export default function ChatBot() {
+  const [date, setDate] = useState(new Date().toLocaleTimeString());
   const [messages, setMessages] = useState([
     {
-      sender: "ai",
-      text: "Hello there! How can I assist you with HR outreach?",
+      sender: "",
+      text: "",
     },
   ]);
   const [userInput, setUserInput] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [workspaces, setWorkspaces] = useState<any>(null);
   const [description, setDescription] = useState<any>([]);
+  const [toolCalling, setToolCalling] = useState<boolean>(false);
+  const [tool, setTool] = useState("");
   const chatBoxRef = useRef<HTMLDivElement>(null);
+
+  // signup flow
+  const [name, setName] = useState<string>("");
+  const [company, setCompany] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+
+  const [newChat, setNewChat] = useState<boolean>(false);
 
   const handleSendMessage = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -32,8 +42,19 @@ export default function ChatBot() {
     setMessages((prev) => [...prev, newMessage]);
 
     setIsLoading(true);
-    socket.emit("human_response", userInput);
+    socket.emit("human_response", { userInput: userInput });
+
     setUserInput("");
+  };
+
+  const handleNewChat = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setNewChat(true);
+    socket.emit("human_response", {
+      name: name,
+      title: title,
+      company: company,
+    });
   };
 
   useEffect(() => {
@@ -46,11 +67,17 @@ export default function ChatBot() {
 
     socket.on("ai_response", (data) => {
       setMessages((prev) => [...prev, { sender: "ai", text: data?.messages }]);
+      socket.on("calling_tool", (data) => {
+        setTool(data?.calling_tool);
+        setToolCalling(true);
+        setIsLoading(false);
+      });
       setWorkspaces(data?.workspace);
-      console.log(data?.workspace);
       setDescription(data?.workspace);
       setIsLoading(false);
     });
+
+    setToolCalling(false);
 
     return () => {
       socket.off("ai_response");
@@ -60,62 +87,106 @@ export default function ChatBot() {
 
   return (
     <>
-      <div className="chat-container">
-        <h4 className="chat-header">Chat with Helix</h4>
+      {newChat ? (
+        <>
+          <div className="chat-container">
+            <h4 className="chat-header">Chat with Helix</h4>
 
-        <div className="message-container" ref={chatBoxRef}>
-          {messages.map((msg, index) => (
-            <div key={index} className={msg.sender}>
-              <p className={`${msg.sender}-name`}>
-                {msg.sender === "human" ? "You" : "Helix"}
-              </p>
-              <p className={`${msg.sender}-message`}>{msg.text}</p>
-              <p className={`time time-${msg.sender}`}>Thursday</p>
-            </div>
-          ))}
-
-          {isLoading && (
-            <div className="typing-indicator">
-              <div className="dot"></div>
-              <div className="dot"></div>
-              <div className="dot"></div>
-            </div>
-          )}
-        </div>
-
-        <form className="chat-input" onSubmit={handleSendMessage}>
-          <input
-            type="text"
-            className="flex-grow p-2 border rounded-lg text-sm"
-            onChange={(e) => setUserInput(e.target.value)}
-            value={userInput}
-            placeholder="Chat with Helix"
-          />
-          <button
-            type="submit"
-            className="p-2 bg-purple-500 text-white rounded-lg"
-          >
-            Send
-          </button>
-        </form>
-      </div>
-      <div className="workspace-container">
-        <h4 className="workspace-header">Workspace</h4>
-        <div>
-          {workspaces && workspaces.length > 0 ? (
-            workspaces.map((step: string, index: number) => (
-              <div className="step-container">
-                <div className="card" key={index}>
-                  <strong>Step {index + 1}: </strong>
-                  {step}
+            <div className="message-container" ref={chatBoxRef}>
+              {messages.map((msg, index) => (
+                <div key={index} className={msg.sender}>
+                  <p className={`${msg.sender}-name`}>
+                    {msg.sender === "human" ? "You" : "Helix"}
+                  </p>
+                  <p className={`${msg.sender}-message`}>{msg.text}</p>
+                  <p className={`time time-${msg.sender}`}>{date}</p>
                 </div>
-              </div>
-            ))
-          ) : (
-            <p>No workspaces available</p>
-          )}
+              ))}
+
+              {toolCalling && (
+                <div className="tool">
+                  <p>
+                    {tool}
+                    <div className="loader"></div>
+                  </p>
+                </div>
+              )}
+              {isLoading && (
+                <p>
+                  Responding
+                  <div className="loader"></div>
+                </p>
+              )}
+            </div>
+
+            <form className="chat-input" onSubmit={handleSendMessage}>
+              <input
+                type="text"
+                className="flex-grow p-2 border rounded-lg text-sm"
+                onChange={(e) => setUserInput(e.target.value)}
+                value={userInput}
+                placeholder="Chat with Helix"
+              />
+              <button
+                type="submit"
+                className="p-2 bg-purple-500 text-white rounded-lg"
+              >
+                Send
+              </button>
+            </form>
+          </div>
+          <div className="workspace-container">
+            <h4 className="workspace-header">Workspace</h4>
+            <div>
+              {workspaces && workspaces.length > 0 ? (
+                workspaces.map((step: string, index: number) => (
+                  <div className="step-container">
+                    <div className="card" key={index}>
+                      <strong>Step {index + 1}: </strong>
+                      {step}
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <p>No workspaces available</p>
+              )}
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="signup-container">
+          <div className="form-box">
+            <h2>Sign Up</h2>
+            <form className="form" onSubmit={handleNewChat}>
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                id="title"
+                onChange={(e) => setTitle(e.target.value)}
+                required
+              />
+
+              <label htmlFor="company">Company</label>
+              <input
+                type="text"
+                id="company"
+                onChange={(e) => setCompany(e.target.value)}
+                required
+              />
+
+              <button type="submit">Sign Up</button>
+            </form>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
